4fb08d14960ae4f50a09c9fd7c0299f61e434d8b
emad <emadabdalrahman11@gmail.com>
2018-11-19 11:00:09 +0200

    stop leakcanary

--- app/src/main/java/com/example/emad/weatherappdemo/WeatherApp.java
+++ app/src/main/java/com/example/emad/weatherappdemo/WeatherApp.java
@@ -2,17 +2,17 @@ package com.example.emad.weatherappdemo;
 
 import android.app.Application;
 
-import com.squareup.leakcanary.LeakCanary;
+//import com.squareup.leakcanary.LeakCanary;
 
 public class WeatherApp extends Application {
     @Override public void onCreate() {
         super.onCreate();
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        LeakCanary.install(this);
-        // Normal app init code...
+//        if (LeakCanary.isInAnalyzerProcess(this)) {
+//            // This process is dedicated to LeakCanary for heap analysis.
+//            // You should not init your app in this process.
+//            return;
+//        }
+//        LeakCanary.install(this);
+//        // Normal app init code...
     }
 }
