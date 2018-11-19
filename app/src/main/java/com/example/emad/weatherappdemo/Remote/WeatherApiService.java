7f6c4758268e1a245b4024ecc8559f359e468617
emad <emadabdalrahman11@gmail.com>
2018-11-19 07:29:45 +0200

    create WeatherApiService to get Forecast and Weather data from openweathermap

--- app/src/main/java/com/example/emad/weatherappdemo/Remote/WeatherApiService.java
+++ app/src/main/java/com/example/emad/weatherappdemo/Remote/WeatherApiService.java
@@ -1,6 +1,6 @@
 package com.example.emad.weatherappdemo.Remote;
 
-import com.example.emad.weatherappdemo.POJOs.CWeather.CurrentWeather;
+import com.example.emad.weatherappdemo.POJOs.CWeather.CityWeather;
 import com.example.emad.weatherappdemo.POJOs.DForecast.DailyForecast;
 
 import retrofit2.Call;
@@ -11,10 +11,10 @@ public interface WeatherApiService  {
 
     String API_KEY = "56a67941c4c376d90bb1a6d9e97e6ff8";
 
-    @GET("forecast/daily?appid="+API_KEY)
+    @GET("forecast/daily?units=metric&appid="+API_KEY)
     Call<DailyForecast> getDailyForecast(@Query("id")String cityID);
 
-    @GET("weather?appid="+API_KEY)
-    Call<CurrentWeather> getCurrentWeather(@Query("id")String cityID);
+    @GET("weather?units=metric&appid="+API_KEY)
+    Call<CityWeather> getCurrentWeather(@Query("id")String cityID);
 
 }
