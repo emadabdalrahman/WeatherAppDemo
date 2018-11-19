280134b2e78383ab45c0f528f9b3b5f7ae36265b
emad <emadabdalrahman11@gmail.com>
2018-11-19 08:02:45 +0200

    create Cached to save response as string in json format inside SharedPreferences

---
+++ app/src/main/java/com/example/emad/weatherappdemo/Cached.java
@@ -0,0 +1,57 @@
+package com.example.emad.weatherappdemo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.SystemClock;
+
+import com.example.emad.weatherappdemo.POJOs.CWeather.CityWeather;
+import com.example.emad.weatherappdemo.POJOs.DForecast.DailyForecast;
+import com.example.emad.weatherappdemo.POJOs.DForecast.Forecast;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class Cached {
+
+    public static final int TYPE_CITY_WEATHER = 0;
+    public static final int TYPE_DAILY_FORECAST = 1;
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+
+    public Cached(Context context) {
+        mSharedPreferences = context.getSharedPreferences("Cached", Context.MODE_PRIVATE);
+        mEditor = mSharedPreferences.edit();
+    }
+
+    public void cachedForecast(DailyForecast dailyForecast) {
+        mEditor.putString(getKey(TYPE_DAILY_FORECAST, dailyForecast.getCity().getId()) + "LastUpdate",
+                String.valueOf(SystemClock.elapsedRealtime()));
+        mEditor.putString(getKey(TYPE_DAILY_FORECAST, dailyForecast.getCity().getId()),
+                new GsonBuilder().create().toJson(dailyForecast));
+        mEditor.apply();
+    }
+
+    public void cachedCityWeather(CityWeather cityWeather) {
+        mEditor.putString(getKey(TYPE_CITY_WEATHER, cityWeather.getId()) + "LastUpdate",
+                String.valueOf(SystemClock.elapsedRealtime()));
+        mEditor.putString(getKey(TYPE_CITY_WEATHER, cityWeather.getId()),
+                new GsonBuilder().create().toJson(cityWeather));
+        mEditor.apply();
+    }
+
+    public String getCached(int type,String id) {
+        return mSharedPreferences.getString(getKey(type,Integer.valueOf(id)), null);
+    }
+
+    private String getKey(int type, int id) {
+        switch (type) {
+            case TYPE_DAILY_FORECAST:
+                return "DailyForecast" + String.valueOf(id);
+            case TYPE_CITY_WEATHER:
+                return "CityWeather" + String.valueOf(id);
+            default:
+                return null;
+        }
+    }
+
+}
