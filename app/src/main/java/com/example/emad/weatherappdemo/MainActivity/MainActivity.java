80c3536ffc9bff7b9d131ece97ba2720c68c5690
emad <emadabdalrahman11@gmail.com>
2018-11-19 08:13:08 +0200

    create MainActivity with Navigation

---
+++ app/src/main/java/com/example/emad/weatherappdemo/MainActivity/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.emad.weatherappdemo.MainActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.emad.weatherappdemo.R;
+
+import androidx.navigation.Navigation;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().setBackgroundDrawable(null);
+
+    }
+
+
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return Navigation.findNavController(this, R.id.nav_host_fragment).navigateUp();
+    }
+
+}
